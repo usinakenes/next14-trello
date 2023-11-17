0e3b463c498626bad145a7a9a71d2cb626b7747a
Umut Sina Kenes <usinakenes1907@gmail.com>
2023-11-17 06:14:18 +0100

    add missing files

---
+++ app/(platform)/(dashboard)/organization/[organizationId]/form.tsx
@@ -0,0 +1,26 @@
+'use client'
+
+import { create } from "@/actions/create-board"
+import { Button } from "@/components/ui/button"
+import { useFormState } from "react-dom"
+import { FormInput } from "./form-input"
+
+
+const Form = () => {
+
+  const initialState = { message: null, errors: {} }
+  const [state, dispatch] = useFormState(create, initialState)
+
+  return (
+    <form
+      action={dispatch}
+    >
+      <div className="flex flex-col space-y-2">
+        <FormInput errors={state?.errors} />
+      </div>
+      <Button type="submit">Submit</Button>
+    </form>
+  )
+}
+
+export default Form
